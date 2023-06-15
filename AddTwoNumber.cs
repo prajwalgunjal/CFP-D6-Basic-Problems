2d6e50314c7da49ea6ff7ef896362ac0e8b40cc7
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-15 10:14:22 +0530

    problem 1 added

---
+++ AddTwoNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Problem_D6
+{
+    internal class AddTwoNumber
+    {
+        public int add(int a,int b, int c )
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Basic_Problem_D6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddTwoNumber addTwoNumber = new AddTwoNumber();
+            int sum =addTwoNumber.add(10,20,30);
+            Console.WriteLine(sum);
+        }
+    }
+}
